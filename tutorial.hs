659c52615ba529eb03328b1cda29b86ca7b8e1ef
gustavo <gustavolp@id.uff.br>
2019-11-01 17:52:54 -0300

    Funcionando até parte do ;

--- tutorial.hs
+++ tutorial.hs
@@ -1,13 +1,12 @@
 programa = ";b;c;a"
 
-pop (x:xs) = xs
-
 --Grafo de exemplo 
 grafo = [(1,2,'a'),(1,3,'b'),(3,4,'c'),(3,5,'a'),(3,6,'b')]
 
 --Retorna as arestas de um nó do grafo
 getArestas grafo noOrigem = [(a,b,c) | (a,b,c)<-grafo, a==noOrigem]
 
+getDestino nosDestino = [x | (_,x,_)<-nosDestino]
 --Retorna se é possível fazer a transicao dado o programa a executar
 transicaoPossivel arestas exec
     | [ 1 | (a,b,c)<-arestas, c== exec] /= [] = True
@@ -16,5 +15,15 @@ transicaoPossivel arestas exec
 percorrerPrograma programa grafo noOrigem = 
  do
    if head programa == ';'
-      then print "coe"
-      else print "tchau"
+      then do
+              let subprograma = head (tail programa)
+              let vizinhos = getArestas grafo noOrigem
+              if transicaoPossivel vizinhos subprograma
+                then do
+                       print vizinhos
+                       let proximoNo = getDestino vizinhos
+                       let continuacao = drop 2 programa
+                       print continuacao
+                      --  let resposta = percorrerPrograma (take 1 proximoNo)
+                else print "XABLAU"
+        else print "False"
