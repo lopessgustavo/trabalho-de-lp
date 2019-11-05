2cb3d2267a45c6aec06950270b33411b5ed6520a
Gustavo Lopes de Paula <gustavolp@id.uff.br>
2019-11-05 10:48:50 -0300

    parte da uniao ta bem confusa

--- tutorial.hs
+++ tutorial.hs
@@ -1,7 +1,21 @@
-programa = ";bU(U(a,c),;aU(b,e))"
+programa = ";bU(U(a,c),;aU(b,e));e"
+prog= ";a;bc" 
 -- programa = ";aU(b,c)
 --Grafo de exemplo 
+-- concat (replicate 3 "coe")
 grafo = [(1,2,'a'),(1,3,'b'),(3,4,'c'),(3,5,'a'),(5,6,'b'),(5,7,'e')]
+
+separarSubPrograma flag programa = 
+  if (head programa) == ')' && ((flag-1) == 0)
+    then let resp = [head programa] in resp 
+    
+    else if (head programa) == '('
+      then (head programa):separarSubPrograma (flag+1) (tail programa)   
+    
+    else if (head programa) == ')'
+      then (head programa):separarSubPrograma (flag-1) (tail programa) 
+    
+    else (head programa):separarSubPrograma flag (tail programa)
 
 separarUniao::Int->[Char]->[Char]
 separarUniao flag programa = 
@@ -37,10 +51,10 @@ percorrerPrograma programa grafo noOrigem =
                       in percorrerPrograma continuacao grafo (head destinos)  
                 else let resp = False in resp
     else if head programa == 'U'
-            then let subprograma = take (length (programa) -3) (drop 2 programa)
-                     subprograma1 = separarUniao 0 subprograma
-                     subprograma2 = reverse (separarUniao 0 (reverse subprograma))
-                  in (percorrerPrograma subprograma1 grafo noOrigem) && ((percorrerPrograma subprograma2 grafo noOrigem)) 
+            then let subprograma = (separarSubPrograma 0 (tail programa))
+                     subprograma1 = separarUniao 0 (drop 1 (init subprograma))
+                     subprograma2 = reverse (separarUniao 0 (reverse (drop 1 (init subprograma))))
+                  in ((percorrerPrograma subprograma1 grafo noOrigem) && ((percorrerPrograma subprograma2 grafo noOrigem)) && (percorrerPrograma (drop (length subprograma) programa) grafo noOrigem) ) 
     else let subprograma = head programa
              vizinhos = getArestas grafo noOrigem
            in transicaoPossivel vizinhos subprograma
